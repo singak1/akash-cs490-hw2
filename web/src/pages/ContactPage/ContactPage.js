96a901140eda9b7891fa3cd1939a32c6504d2e7f
singak1 <a97sin@gmail.com>
2023-01-27 14:43:16 -0500

    Set up frontend to allow contact-form logging into db

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,9 +1,23 @@
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags, useMutation } from '@redwoodjs/web'
 import { FieldError, Form, Label, TextField, TextAreaField, Submit } from '@redwoodjs/forms'
 
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+
 const ContactPage = () => {
+  const [create] = useMutation(CREATE_CONTACT)
+
   const onSubmit = (data) => {
-    console.log(data)
+    create({
+      variables: {
+        input: data
+      },
+    })
   }
   return (
     <>
